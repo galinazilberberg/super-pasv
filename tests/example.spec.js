a133fd8b166f202efb9ce731fb1663c3e7104e2d
Galina zilberberg <yinyangyogaloft@gmail.com>
2021-07-24 16:43:01 -0400

    example tests

---
+++ tests/example.spec.js
@@ -0,0 +1,13 @@
+import {expect} from "chai";
+
+describe('operation with numbers',function (){
+    const a = 5;
+    const b = 7;
+    it('addition works correctly', function (){
+        expect(a+b,'result should be equal').to.equal(12);
+    });
+
+    it('substruction works correctly', function (){
+        expect(b-a,'result should be equal').to.equal(2);
+    });
+});
